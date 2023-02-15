bf70847b66ea768eeef5f4a5b8d98f251366ecd3
fahimghl <fahim.ghl@hotmail.co.uk>
2023-02-15 15:12:08 +0000

    Complete vpc setup -  terraform init ran succesfully for both modules

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
+module "vpc" {
+  source = "/modules/networking"
+
+  name               = var.vpc_name
+  cidr_block         = var.cidr_block
+  availability_zones = var.availability_zones
+}
 module "mysqlDb" {
   source  = "terraform-aws-modules/rds/aws"
-  version = "5.7"
+  version = "5.4"
 }
+
